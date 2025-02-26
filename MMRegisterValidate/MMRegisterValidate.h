2ed2878a9216075ee087bce568bddd9c97c16e0e
Mahesh12123 <mahararay@gmail.com>
2025-02-26 11:45:44 +0545

    Initial Commit

---
+++ MMRegisterValidate/MMRegisterValidate.h
@@ -0,0 +1,18 @@
+//
+//  MMRegisterValidate.h
+//  MMRegisterValidate
+//
+//  Created by mahesh mahara on 26/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMRegisterValidate.
+FOUNDATION_EXPORT double MMRegisterValidateVersionNumber;
+
+//! Project version string for MMRegisterValidate.
+FOUNDATION_EXPORT const unsigned char MMRegisterValidateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMRegisterValidate/PublicHeader.h>
+
+
